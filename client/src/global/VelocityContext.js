33c437de0af2dbdf5d79abc8ea753578fac71a62
Pernilla Göth <pg222jx@student.lnu.se>
2021-05-17 16:28:20 +0200

    Velocity points is changed on cards without alternatives, cards.json file is completed

--- client/src/global/VelocityContext.js
+++ client/src/global/VelocityContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from 'react'
 export const VelocityContext = createContext()
 
 export const VelocityProvider = (props) => {
-  const [currentVelocity, setCurrentVelocity] = useState(10)
+  const [currentVelocity, setCurrentVelocity] = useState(5)
   return (
     <VelocityContext.Provider value={{ currentVelocity, setCurrentVelocity }}>
       {props.children}
